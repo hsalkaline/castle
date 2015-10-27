ac0f352baac4d31ff078753337d169dc31930306
hsalkaline <hsalkaline@gmail.com>
2015-10-27 22:53:35 +0000

    Emitter, Logger

--- server/app.js
+++ server/app.js
@@ -1,7 +1,20 @@
 var Game = require('logic/Game/Game'),
     Player = require('logic/Player/Player'),
+    Card = require('logic/Card/Card'),
     ResourceBag = require('logic/Resources/ResourceBag/ResourceBag'),
     assert = require('assert'),
     util = require('util');
 
-ResourceBag.getVariants(7);
+var Alice = new Player(),
+    Bob = new Player(),
+    game = new Game({
+        maxPlayers: 2
+    });
+
+game.join(Alice);
+game.join(Bob);
+game.start();
+
+
+Alice.play(Card.Messenger);
+Bob.play(Card.Messenger);
