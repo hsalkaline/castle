deb30d0a309f465fe96d5952895691001d0a1fce
hsalkaline <hsalkaline@gmail.com>
2015-10-27 22:02:44 +0000

    исправил ошибки в путях модулей, убрал цены у серебра и денег

--- server/app.js
+++ server/app.js
@@ -1,4 +1,7 @@
-var Game = require('logic/game/Game'),
-    Player = require('logic/player/Player'),
+var Game = require('logic/Game/Game'),
+    Player = require('logic/Player/Player'),
+    ResourceBag = require('logic/Resources/ResourceBag/ResourceBag'),
     assert = require('assert'),
     util = require('util');
+
+ResourceBag.getVariants(7);
